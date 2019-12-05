780f0842da73485fcad80744f8f296d63ca0bdae
Jad Nohra <jadnohra@gmail.com>
2019-12-05 21:13:37 +0100

    Added vehicle dynamics models prototyping notebook

--- dev/setup_venv.sh
+++ dev/setup_venv.sh
@@ -1,5 +1,5 @@
 virtualenv --python=$(which python3) venv
-virtualenv --python=$(which python3) venv-jupyter
+virtualenv --python=$(which python3) venv_jupyter
 source ./activate_venv.sh
 pip install -r ./requirements.txt
 deactivate
