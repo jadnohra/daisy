0ef37b95cac35a35daed697bf315481bf64e0a03
Jad Nohra <jadnohra@gmail.com>
2021-02-19 13:52:06 +0100

    Improved venv setup

--- dev/setup_venv.sh
+++ dev/setup_venv.sh
@@ -1,5 +1,5 @@
 virtualenv --python=$(which python3) venv
 virtualenv --python=$(which python3) venv_jupyter
 source ./activate_venv.sh
-pip install -r ./requirements.txt
+pip3 install -r ./requirements.txt
 deactivate
